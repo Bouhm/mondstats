7f5ec85d2b440cb1fc521b29642447330543af71
Bouhm <bbouhm@gmail.com>
2021-03-29 23:07:07 -0700

    upd

---
+++ src/components/characters/Character.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import './Character.css'
+
+function Character() {
+
+  return (
+    <div className="character-container">
+      <div className="character-portait" />
+      <div className="character-artifacts">
+      </div>
+      <div className="character-constellations">
+      </div>
+    </div>
+  )
+}
+
+export default Character
