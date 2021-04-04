ffabc24aa0be4201102586270669700f684ab31d
Bouhm <bbouhm@gmail.com>
2021-04-04 15:05:18 -0700

    add sample data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
+import Character from './components/characters/Character'
+import data from "./sample.json"
 import './App.css'
 
 function App() {
-
   return (
     <div className="App">
+      <Character {...data[0]} />
     </div>
   )
 }
--- src/components/characters/Character.tsx
+++ src/components/characters/Character.tsx
@@ -1,12 +1,28 @@
 import React, { useState } from 'react'
+import _ from 'lodash'
+import artifactDb from '../../data/artifacts.json'
+import weaponDb from '../../data/weapons.json'
+import characterDb from '../../data/characters.json'
 import './Character.css'
 
-function Character() {
+type CharacterProps = {
+  id: number,
+  artifacts: any[],
+  weapons: any[]
+}
+
+function Character({id, artifacts, weapons}: CharacterProps) {
+  const character = _.find(characterDb, { id })
 
   return (
     <div className="character-container">
-      <div className="character-portait" />
+      <div className="character-portrait" style={{backgroundImage: `url("${character!.image.replace('@2x', '')}")`}}/>
       <div className="character-artifacts">
+      </div>
+      <div className="character-weapons">
+        {_.find(weaponDb, weapon => {
+          
+        })}
       </div>
       <div className="character-constellations">
       </div>
