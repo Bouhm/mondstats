5dabad9eb915ea25efbedba1f560abbc6f29cfed
Bouhm <bbouhm@gmail.com>
2021-04-11 17:47:49 -0700

    character tile styles

--- src/components/CharacterTile.tsx
+++ src/components/CharacterTile.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import _ from 'lodash'
 
 import characterDb from '../data/characters.json'
+import { ElementIcons } from '../data/constants'
+
 import './CharacterTile.css'
 
 type CharacterTileProps = {
@@ -14,9 +16,15 @@ function CharacterTile({ id }: CharacterTileProps) {
 
   if (!character) return null;
 
+  let classes = "character-tile";
+  classes += ` rarity-${character.rarity}`;
+
+  const charElement = ElementIcons[character.element];
+
   return (
-    <div className="character-tile">
+    <div className={classes}>
       <img src={character.icon} alt={`${character.name}-portrait`}></img>
+      {charElement && <img className="element-icon" src={ElementIcons[character.element]} alt={character.element}></img>}
     </div>
   )
 }
---
+++ src/data/constants.ts
@@ -0,0 +1,10 @@
+type ElementIconsDict = { [element: string]: string }
+
+export const ElementIcons: ElementIconsDict = {
+  Anemo: "https://static.wikia.nocookie.net/gensin-impact/images/a/a4/Element_Anemo.png",
+  Geo: "https://static.wikia.nocookie.net/gensin-impact/images/4/4a/Element_Geo.png",
+  Hydro: "https://static.wikia.nocookie.net/gensin-impact/images/3/35/Element_Hydro.png",
+  Electro: "https://static.wikia.nocookie.net/gensin-impact/images/7/73/Element_Electro.png",
+  Pyro: "https://static.wikia.nocookie.net/gensin-impact/images/e/e8/Element_Pyro.png",
+  Cryo: "https://static.wikia.nocookie.net/gensin-impact/images/8/88/Element_Cryo.png"
+}
