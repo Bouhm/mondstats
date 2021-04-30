43e9aed26a7bd7eb4c1deb9f00ba04cd09a56ed4
Bouhm <bbouhm@gmail.com>
2021-04-30 00:22:13 -0700

    restructure components

--- src/components/builds/CharacterPage.tsx
+++ src/components/builds/CharacterPage.tsx
@@ -8,7 +8,7 @@ import './CharacterPage.css'
 import { ChevronUpSharp } from 'react-ionicons'
 import ArtifactBuild from './ArtifactBuild'
 
-function CharacterBuild({ id, name, constellations, weapons, artifacts }: ICharData) {
+function CharacterBuildStats({ id, name, constellations, weapons, artifacts }: ICharData) {
   const [{ characterDb }] = useContext(Store)
   const [activeBuild, setActiveBuild] = useState(false)
   const character = characterDb[id]
@@ -16,11 +16,7 @@ function CharacterBuild({ id, name, constellations, weapons, artifacts }: ICharD
 
   return (
     <div className="character-container" style={{ backgroundImage: `url("${character.image}")` }}>
-      <div className="character-artifacts">
-        {_.map(artifacts, artifact => <ArtifactBuild {...artifact} />)}
-      </div>
-      <div className="character-weapons">
-      </div>
+      <CharacterBuild weaponId={} artifactSets={} />
       <div className="character-constellations">
       </div>
     </div>
@@ -30,15 +26,15 @@ function CharacterBuild({ id, name, constellations, weapons, artifacts }: ICharD
 function CharacterPage() {
   const { characterName } = useParams<{ characterName: string }>();
   const [{ characterIdMap, characterBuilds }] = useContext(Store)
-  const [build, setBuild] = useState<ICharData | undefined>(undefined)
+  const [builds, setBuilds] = useState<ICharData | undefined>(undefined)
 
   useEffect(() => {
-    setBuild(characterBuilds[characterIdMap[characterName]]);
+    setBuilds(characterBuilds[characterIdMap[characterName]]);
   }, [characterIdMap, characterName, characterBuilds])
 
   return (
     <div className="character-page">
-      {build && <CharacterBuild {...build} />}
+      {builds && <CharacterBuildStats {...builds} />}
     </div>
   )
 }
