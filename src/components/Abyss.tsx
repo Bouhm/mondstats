f66e3ed550716f5298f5cdbf6a10804970abf3da
Bouhm <bbouhm@gmail.com>
2021-05-24 08:55:53 -0700

    abyss presentation

--- src/components/Abyss.tsx
+++ src/components/Abyss.tsx
@@ -10,27 +10,47 @@ import CharacterTile from './CharacterTile';
 function Abyss(abyss: IAbyssData) {
   const [{ selectedCharacter, characterDb }] = useContext(Store)
   const [ floorNum, setFloorNum ] = useState<string>('9')
+  let teamCount = 0;
 
-  const renderParties = (stage: [IBattle, IBattle]) => {      
-    return _.map(stage, ({teams, total}) =>{
-      return _.map(_.take(_.orderBy(_.filter(teams, team => team.party.includes(parseInt(selectedCharacter))), 'count', 'desc'), 5), ({party, count}) => (
-        <div className="party-container">
-          <div className="party-characters">
-            {_.map(party, (char, i) => ( 
-              <CharacterTile id={char+''} key={`party-${i}`} />
-            ))}
-          </div>
-          <div className="party-popularity">{count}</div>
+  const renderParties = (stageNum: string) => (   
+    <div className="stage-container">
+      <h2 className="stage-label">{floorNum}-{stageNum}</h2>
+      <div className="stage-half">
+        {_.map(abyss[floorNum][stageNum], ({teams}, i) => {
+            return (
+              <div key={`battle-${i}`} className="battle-container">
+                <h2>{i+1}{i+1 === 1 ? 'st' : 'nd'} Half</h2>
+                {_.map(_.take(_.orderBy(_.filter(teams, team => {
+                    if (team.party.includes(parseInt(selectedCharacter))) {
+                      teamCount++
+                      return true
+                    } else {
+                      return false
+                    }
+                  }), 'count', 'desc'), 5), ({party, count}, j) => (
+                    <div key={`party-${i}-${j}`} className="party-container">
+                      <div className="party-characters">
+                        {_.map(party, (char, i) => ( 
+                          <CharacterTile id={char+''} key={`party-${i}`} />
+                        ))}
+                      </div>
+                      <div className="party-popularity">{Math.round((count/teamCount * 10) / 10 * 100)}%</div>
+                    </div>
+                  ))
+                }
+              </div>
+            )
+          })}
         </div>
-      ))
-    })
-  }
+        <div className="stage-teams-show-more">Show more</div>
+    </div>
+  )
 
   return (
     <div className="abyss-container">
       <h1>Party Members</h1>
       <div className="floor-container">
-       {_.map(abyss[floorNum], renderParties)}
+       {_.map(_.keys(abyss[floorNum]), renderParties)}
       </div>
       <h1>Abyss Floors</h1>
     </div>
