586c86a83de569651cb8edebc05bd3ff65ff2887
Bouhm <bbouhm@gmail.com>
2021-06-24 22:36:26 -0700

    setup apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,67 @@
 import './App.css';
 
-import _ from 'lodash';
 import React, { useContext, useEffect, useState } from 'react';
-import { Link, Redirect, Route, Switch, useLocation } from 'react-router-dom';
+import { Link, Redirect, Route, Switch } from 'react-router-dom';
+
+import { gql, useQuery } from '@apollo/client';
 
 import About from './components/About';
 import Changelog from './components/Changelog';
-import CharacterPage from './components/CharacterPage';
 import CharacterSearch from './components/CharacterSearch';
 import Navbar from './components/navbar/Navbar';
-import Dropdown, { Option } from './components/ui/Dropdown';
-import artifactDb from './data/artifacts.json';
-import characterDb from './data/characters.json';
-import allData from './data/data.json';
-import { IData } from './data/types';
-import weaponDb from './data/weapons.json';
 import { getShortName } from './scripts/util';
 import { Store } from './Store';
 
+const Query = gql`
+  query GetAllData { 
+    characters {
+      name
+    }
+  } 
+`;
+
 function App() {
   const [, dispatch] = useContext(Store)
+  const [data, setData] = useState([])
 
   useEffect(() => {
-    let charIdMap: { [shortname: string]: string } = {}
-    _.forEach(_.values(characterDb), char => {
-      const charName = char.name === "Traveler" ? getShortName(`${char.name}-${char.element}`) : getShortName(char.name);
-      charIdMap[charName] = char.id + '';
-    });
+    // let charIdMap: { [shortname: string]: string } = {}
+    // _.forEach(_.values(characterDb), char => {
+    //   const charName = char.name === "Traveler" ? getShortName(`${char.name}-${char.element}`) : getShortName(char.name);
+    //   charIdMap[charName] = char.id + '';
+    // });
 
-    dispatch({ type: "SET_ARTIFACT_DB", payload: artifactDb })
-    dispatch({ type: "SET_WEAPON_DB", payload: weaponDb })
-    dispatch({ type: "SET_CHARACTER_DB", payload: characterDb })
-    dispatch({ type: 'SET_CHARACTER_ID_MAP', payload: charIdMap })
-  }, [characterDb, artifactDb, weaponDb, getShortName, dispatch])
+    // dispatch({ type: "SET_ARTIFACT_DB", payload: artifactDb })
+    // dispatch({ type: "SET_WEAPON_DB", payload: weaponDb })
+    // dispatch({ type: "SET_CHARACTER_DB", payload: characterDb })
+    // dispatch({ type: 'SET_CHARACTER_ID_MAP', payload: charIdMap })
+  }, [getShortName, dispatch])
 
-  const options = [
-    { label: "All", value: "all" },
-    { label: "Cleared Spiral Abyss", value: "abyssClears" },
-    { label: "Mains", value: "mains" }
-  ]
+  function Test() {
+    const { loading, error, data } = useQuery(Query);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
 
-  const [data, setData] = useState<IData>(allData.all as unknown as IData)
-  const location = useLocation();
-
-  useEffect(() => {
-    setData(allData.all as unknown as IData)
-  }, [location, setData, allData])
-
-
-  const handleSelect = (selected: Option) => {
-    let data = allData.all;
-    switch (selected.value) {
-      case "all":
-        data = allData.all;
-        break;
-      case "mains":
-        data = allData.mains;
-        break;
-      case "abyssClears":
-        data = allData.abyssClears;
-        break;
-      default:
-        break;
-    }
-
-    setData(data as unknown as IData);
+    return (
+      <div>
+        <p>
+          {data}
+        </p>
+      </div>
+    );
   }
+
 
   const renderCharacterSearch = () => <CharacterSearch dataTotal={2006} />
   const renderCharacterPage = () => {
     return (
       <>
-        <div className="character-filter"><Dropdown options={options} onChange={handleSelect} defaultValue={options[0]} /></div>
-        <CharacterPage data={data} />
+        {/* <div className="character-filter"><Dropdown options={options} defaultValue={options[0]} /></div> */}
+        {/* <CharacterPage data={data} /> */}
       </>
     )
   }
-  
+
   return (
     <div className="App">
       <Navbar />
@@ -84,10 +69,11 @@ function App() {
         <Switch>
           <Route path="/about" component={About} />
           <Route path="/changelog" component={Changelog} />
-          <Route exact path="/" render={renderCharacterSearch}/>
+          <Route exact path="/" render={renderCharacterSearch} />
           <Route path="/builds/:shortName" render={renderCharacterPage} />
           <Redirect exact path="/builds" to="/" />
         </Switch>
+        <Test />
       </section>
       <section>
         <div className="links">
--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
 import App from './App';
 import { StoreProvider } from './Store';
 
+const client = new ApolloClient({
+  uri: 'https://favonius-server.herokuapp.com/graphql',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <StoreProvider>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <Router>
+        <StoreProvider>
           <App />
-      </StoreProvider>
-    </Router>
-  </React.StrictMode>,
+        </StoreProvider>
+      </Router>
+    </React.StrictMode>
+  </ApolloProvider >,
   document.getElementById('root')
 )
