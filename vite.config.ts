819dfaa0001d3f0439d13ac3b08c483fdf9e0102
Bouhm <bbouhm@gmail.com>
2021-07-28 09:13:02 -0700

    ,

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,5 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), mdPlugin({ mode: [Mode.HTML, Mode.REACT ] })],
-  define: {
-    'process.env': process.env
-  }
+  plugins: [reactRefresh(), mdPlugin({ mode: [Mode.HTML, Mode.REACT ] })]
 })
