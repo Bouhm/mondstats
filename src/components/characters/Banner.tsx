3c92dce787084dc3dd221cc899ee6ae464d96acb
Bouhm <bbouhm@gmail.com>
2021-08-22 01:26:40 -0700

    fix?

--- src/components/characters/Banner.tsx
+++ src/components/characters/Banner.tsx
@@ -1,6 +1,6 @@
 import './Banner.scss'
 
-import { find, map } from 'lodash';
+import { find, isEmpty, map } from 'lodash';
 import React from 'react';
 import { useAppSelector } from '../hooks/useRedux';
 import Card from '../ui/Card';
@@ -9,12 +9,14 @@ import { useHistory } from 'react-router-dom';
 
 function Banner() {
   const characterDb = useAppSelector((state) => state.data.characterDb)
-  const bannerCharacters = ['Yoimiya', 'Xinyan', 'Sayu',  'Diona'];
+  const bannerCharacters = ['Yoimiya', 'Xinyan', 'Sayu', 'Diona'];
   const routerHistory = useHistory();
 
   const handleClick = (shortname: string) => {
     routerHistory.push(`/builds/${shortname}`)
   }
+
+  if (isEmpty(characterDb)) return null;
 
   return (
     <div className='banner-container'>
@@ -22,8 +24,9 @@ function Banner() {
       <div className='banner-cards'>
         {map(bannerCharacters, name => {
           const character = find(characterDb, char => char.name === name);
-          return <div className="banner-card">
-            <Card imgPath='characters' onClick={() => handleClick(getShortName(character!))} {...character!} />
+          if (!character) return null;
+          return <div key={`banner-${name}`} className="banner-card">
+            <Card imgPath='characters' onClick={() => handleClick(getShortName(character))} {...character} />
           </div>
         })}
       </div>
