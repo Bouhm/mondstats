a1821c496ad3fa467e8d816464f1046997c16713
Bouhm <bbouhm@gmail.com>
2021-08-25 15:10:05 -0700

    changes

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,13 +1,14 @@
 import './Loader.scss';
 
-import Logo from '/assets/logo_m.webp';
+import loader from '/assets/loader.webp';
 import React from 'react';
-import LLImage from './LLImage'
+
+import LLImage from './LLImage';
 
 const Loader = () => {
   return (
     <div className="loader">
-      <LLImage className="logo" src={Logo} alt="logo" />
+      <LLImage className="logo" src={loader} alt="logo" />
       Loading&hellip;
     </div>
   )
