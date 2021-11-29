2060e83be12867470f36bd15a5b8125b568b9934
Bouhm <bbouhm@gmail.com>
2021-11-28 18:25:39 -0800

    fix dis

--- src/components/hooks/useArtifactSetSearch.ts
+++ src/components/hooks/useArtifactSetSearch.ts
@@ -3,19 +3,21 @@ import { filter, flatten, includes, map, reduce, uniq } from 'lodash';
 import { KEYWORDS } from '../controls/Searchbar';
 
 function useArtifactSetSearch(artifactSetDb: any, data: any) {
-  const searchArtifactSets = map(uniq(reduce(data, (arr, curr) => flatten([...arr, ...map(curr.artifacts, artifact => artifact._id)]) as unknown as any, [])), (_id) => {
+  const searchArtifactSets = filter(map(uniq(reduce(data, (arr, curr) => flatten([...arr, ...map(curr.artifacts, artifact => artifact._id)]) as unknown as any, [])), (_id) => {
     const set = artifactSetDb[_id]
-    console.log(set)
 
-    return ({
-      _id,
-      name: set.name,
-      rarity: set.rarity,
-      keys: uniq(flatten(map(set.affixes, affix => (
-        filter(KEYWORDS, key => includes(affix.effect.toLowerCase(), key))
-      )))).join(" ")
-    })
-  });
+    if (set) {
+      return ({
+        _id,
+        name: set.name,
+        rarity: set.rarity,
+        keys: uniq(flatten(map(set.affixes, affix => (
+          filter(KEYWORDS, key => includes(affix.effect.toLowerCase(), key))
+        )))).join(" ")
+      })
+    }
+  }), (set: any) => !!set);
+
   return { searchArtifactSets }
 }
 
