95fedeeee953b19fff6e5192e490042ddd71e880
Bouhm <bbouhm@gmail.com>
2021-12-19 01:57:37 -0800

    artifact set page WIP

--- src/components/artifact-sets/ArtifactSetPage.tsx
+++ src/components/artifact-sets/ArtifactSetPage.tsx
@@ -1,4 +1,4 @@
-import './ArtifactSetPage.css';
+import './ArtifactSetPage.scss';
 
 import { find, forEach, isEmpty, map, orderBy, reduce, take } from 'lodash';
 import React, { useState } from 'react';
@@ -32,28 +32,29 @@ function ArtifactSetPage() {
  
   const [activeBuildIdx, setActiveBuildIdx] = useState(0);
   const artifactSetStats = useApi(`/artifactSets/${artifactSet._id}.json`)
-
   if (!artifactSetStats || isEmpty(artifactSetDb) || isEmpty(artifactSetStats)) return <Loader />
 
-  const charsTotal = reduce(artifactSetStats.characters, (sum, curr) => sum + curr.count, 0)
+
+  const charsTotal = reduce(artifactSetStats.artifactSetBuilds[activeBuildIdx].characters, (sum, curr) => sum + curr.count, 0)
   const max = 10;
   let labels: string[] = [];
   let data: number[] = [];
   let colors: string[] = [];
   let countSum = 0; 
 
-  forEach(artifactSetStats.artifactSetBuilds, ({ artifactSetBuildId, total }) => {
-    const label = getArtifactSetNames(artifactSetBuildDb[artifactSetBuildId].sets, artifactSetDb)
+  forEach(artifactSetStats.artifactSetBuilds, ({ _id, count }) => {
+    const label = getArtifactSetNames(artifactSetBuildDb[_id].sets, artifactSetDb)
+    console.log(label, count)
     if (!label) {
       return;
-  }
+    }
     
     colors = Array(10).fill(colorVars.none)
     colors[activeBuildIdx] = colorVars[colorClass.toLowerCase()];
 
     labels.push(label);
-    data.push(total);
-    countSum += total;
+    data.push(count);
+    countSum += count;
   })
 
 
@@ -62,22 +63,23 @@ function ArtifactSetPage() {
   }
 
   return (
-    <div className="artifact-set-page">
-        <div className="artifact-set-builds-selector">
+  <div className="artifact-set-page">
+    <div className="artifact-set-builds-selector">
         <ScrollContainer vertical={false} hideScrollbars={true} className="artifact-set-builds-menu">
           {map(artifactSetStats.artifactSetBuilds, (build, i: number) => {
               return (
                 <div key={`artifacts-thumb-${i}`} onClick={() => handleSelectSet(i)}>
-                  <ArtifactSetBuildCard id={build.artifactSetBuildId}selected={i === activeBuildIdx} selector={true} />
+                  <ArtifactSetBuildCard id={build._id}selected={i === activeBuildIdx} selector={true} />
                 </div>
               )
             })
           }
         </ScrollContainer>
       </div>
+      <div className="artifact-set-charts">
       <div className="artifact-set-characters">
         <h1>Characters</h1>
-        <HorizontalBarChart data={take(orderBy(artifactSetStats.characters, 'count', 'desc'), expanded ? max : 5) as unknown as IBarChartData[]} db={characterDb} path='characters' total={charsTotal} />
+        <HorizontalBarChart data={take(orderBy(artifactSetStats.artifactSetBuilds[activeBuildIdx].characters, 'count', 'desc'), expanded ? max : 5) as unknown as IBarChartData[]} db={characterDb} path='characters' total={charsTotal} />
         <br />
         {artifactSetStats.length > 5 && (
           <Button className="characters-show-more" onClick={handleExpand}>
@@ -85,11 +87,11 @@ function ArtifactSetPage() {
           </Button>
         )}
       </div>
-      <div className="build-details">
+      <div className="artifact-set-build-details">
         <div className="artifact-set-build-stats-container">
           <div className="artifact-set-build-stats">
             <ArtifactSetBuildDetail
-              id={artifactSetStats.artifactSetBuilds[activeBuildIdx].artifactSetBuildId}
+              id={artifactSetStats.artifactSetBuilds[activeBuildIdx]._id}
             />
             <div className="artifact-set-builds-donut-container">
             <div className="artifact-set-builds-donut-chart">
@@ -102,9 +104,10 @@ function ArtifactSetPage() {
                   showScale={false}
                 />
               </div>
-              <div className="artifact-popularity">{getPercentage(artifactSetStats.artifactSetBuilds[activeBuildIdx].total, countSum)}%</div>
+              <div className="artifact-popularity">{getPercentage(artifactSetStats.artifactSetBuilds[activeBuildIdx].count, countSum)}%</div>
             </div>
           </div>
+        </div>
         </div>
       </div>
     </div>
