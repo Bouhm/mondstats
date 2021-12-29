61527ac2be9fb09f60ef3d2f4837e4c6c54ef9b3
Bouhm <bbouhm@gmail.com>
2021-12-29 15:31:30 -0800

    change wording

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -38,7 +38,7 @@ function Home() {
       <div style={{color: 'white', textAlign: 'center', fontSize: '0.8rem'}}>
         <br />
         HoYoLAB no longer includes data for Abyss team or characters owned.<br />
-        Data cannot be updated until this change is reverted.<br />
+        Data cannot be updated until this issue is resolved by Mihoyo.<br />
         Last updated 11/25/21
         
       </div>
