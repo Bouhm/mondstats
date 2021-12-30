80b22c3ebe7d77174100893904155c6ab5907c6a
Bouhm <bbouhm@gmail.com>
2021-12-30 14:19:08 -0800

    switch to main api

--- src/components/hooks/useApi.ts
+++ src/components/hooks/useApi.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 const useApi = (path: string) => {
-  // const apiBase = 'https://bouhm.github.io/mondstats-data'
-  const apiBase = 'https://raw.githubusercontent.com/bouhm/mondstats-data/develop'
+  const apiBase = 'https://bouhm.github.io/mondstats-data'
+  // const apiBase = 'https://raw.githubusercontent.com/bouhm/mondstats-data/develop'
   const [data, setData] = useState<any>(undefined)
 
   useEffect(() => {
